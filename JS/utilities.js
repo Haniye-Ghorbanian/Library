b7f4c86f07f18a36fb379101f8062da8e2c9b37c
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2023-09-25 18:54:11 +0330

    accordion for filter added

---
+++ JS/utilities.js
@@ -0,0 +1,14 @@
+function persianToEnglish(word) {
+   if (word === 'تاریخ') {
+    return 'history'
+   } else if (word === 'فلسفه') {
+    return 'philosiphy'
+   } else if (word === 'شعر'){
+    return 'poetry  '
+   } else if(word === 'عاشقانه'){
+    return 'romantic'
+   } else if (word === 'درام') {
+    return 'dram'
+   }
+}
+
