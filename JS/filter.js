f5bb9f9f14e6f53ccaab3054710ba12baf4a2e40
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2023-09-27 18:41:11 +0330

    some fixes added to language filter

--- JS/filter.js
+++ JS/filter.js
@@ -1,10 +1,14 @@
 // Models
 let GENRE_CHECKBOXES;
 let genreOptions;
+let filteredBooksOnLanguage;
 let generalFilteredBooks = [];
 let filterOnGenre = [];
 let filterOnLanguage = [];
 let filterOnYear = [];
+
+
+
 
 function allCategoriesRender() {
     genres      = [...new Set(BOOKS.map(book => book.genre))];
@@ -27,6 +31,13 @@ function allCategoriesRender() {
         });
 }
 
+
+
+
+
+
+
+// Creating options (checkboxes) for genre and language filter ::start
 function genreCategoryMaker(genres) {
     return new Promise((resolve) => {
         genreOptions = genres.map(genre => {
@@ -56,42 +67,73 @@ function languageCategoryMaker(languages) {
         resolve();
     });
 }
+// Creating options (checkboxes) for genre and language filter ::end
 
 
 
+
+
+
+
+
+
+
+
+// Getting Dom nodes of checkbox inputs for genre and language filter::start
 function selectGenreCheckboxes() {
     GENRE_CHECKBOXES = Array.from(document.querySelectorAll('.GENRE_CHECKBOXES'));
 }
 
-
-
 function selectLanguageCheckboxes() {
     LANGUAGE_CHECKBOXES = Array.from(document.querySelectorAll('.LANGUAGE_CHECKBOXES'));
-    console.log(LANGUAGE_CHECKBOXES);
 }
+// Getting Dom nodes of checkbox inputs for genre and language filter::end
 
 
 
+
+
+
+
+
+
+
+
+// Adding eventListeners on genre and language inputs::start
 function identifyGenreCheckbox() {
     GENRE_CHECKBOXES.map(checkbox => checkbox.addEventListener('change', findBooksOnGenre))
 }
 
-
 function identifyLanguageCheckbox() {
     LANGUAGE_CHECKBOXES.map(checkbox => checkbox.addEventListener('change', findBooksOnLanguage))
 }
+// Adding eventListeners on genre and language inputs::end
 
 
+
+
+
+
+
+
+
+
+
+// Finfing related books from original or model arrays and storing them in model related model arrays::start
+
+// For genre filter::start
 function findBooksOnGenre(event) {
-    debugger
+    // debugger
     switch (event.target.checked) {
         case true:
             if(filterOnLanguage.length !== 0) {
              displayBooksOnLanguage(filterOnLanguage)          
-            } else {
+            } 
+            else {
                 const filteredBooksOnGenre = BOOKS.filter(book => persianToEnglish(book.genre) === event.target.getAttribute('id'));
                 filteredBooksOnGenre.forEach(filteredBook => filterOnGenre.push(filteredBook));
-                displayBooksOnGenre(filterOnGenre);
+                filteredBooksOnGenre.forEach(filteredBook => generalFilteredBooks.push(filteredBook));
+                displayFilteredBooks(generalFilteredBooks);
             }
             break;
 
@@ -100,27 +142,43 @@ function findBooksOnGenre(event) {
             break;
     }
 }
+// For genre filter::end
 
 
-
-
+// For language filter::start
 function findBooksOnLanguage(event) {
-    debugger
+    
+    // debugger
     switch (event.target.checked) {
         case true:
-            switch (filterOnGenre.length === 0) {
-               case true: 
-                    const filteredBooksOnLanguageNoGenre = BOOKS.filter(book => persianToEnglish(book.language) === event.target.getAttribute('id'));
-                    filteredBooksOnLanguageNoGenre.forEach(filteredBook => filterOnLanguage.push(filteredBook));
-                    displayBooksOnLanguage(filterOnLanguage);
-                    break;
 
-                case false:
-                    const filteredBooksOnLanguageWithGenre = filterOnGenre.filter(book => persianToEnglish(book.language) === event.target.getAttribute('id'));
-                    filteredBooksOnLanguageWithGenre.forEach(filteredBook => filterOnLanguage.push(filteredBook));
-                    displayBooksOnLanguage(filterOnLanguage);
-                    break;
+            if (filterOnGenre.length === 0) {
+                filteredBooksOnLanguage = BOOKS.filter(book => persianToEnglish(book.language) === event.target.getAttribute('id'));
+
+                filteredBooksOnLanguage.forEach(filteredBook => filterOnLanguage.push(filteredBook));
+                console.log(filterOnLanguage);
+
+                const filteredBooksOnLanguageNoGenre = BOOKS.filter(book => persianToEnglish(book.language) === event.target.getAttribute('id'));
+
+                filteredBooksOnLanguageNoGenre.forEach(filteredBook => generalFilteredBooks.push(filteredBook));
+
+                displayFilteredBooks(generalFilteredBooks);
             }
+
+            else if(filterOnGenre.length !== 0) {
+                const filteredBooksOnLanguageWithGenre = generalFilteredBooks.filter(book => persianToEnglish(book.language) === event.target.getAttribute('id'));
+
+                filteredBooksOnLanguage = BOOKS.filter(book => persianToEnglish(book.language) === event.target.getAttribute('id'));
+
+                filteredBooksOnLanguage.forEach(filteredBook => filterOnLanguage.push(filteredBook));
+                console.log(filterOnLanguage)
+
+                generalFilteredBooks = [];
+                filteredBooksOnLanguageWithGenre.forEach(filteredBook => generalFilteredBooks.push(filteredBook));
+                
+                displayFilteredBooks(generalFilteredBooks);
+            }  
+            
             break;
 
         case false:
@@ -128,32 +186,66 @@ function findBooksOnLanguage(event) {
             break;
     }
 }
+// For language filter::end
+
+// Finfing related books from original or model arrays and storing them in model related model arrays::end
 
 
 
+
+
+
+
+
+
+
+// controlling unchecked inputs for genre and language filters::start
+
+// For genre filters::start
 function deleteUncheckedGenre(uncheckedGenre) {
 
-    if(filterOnLanguage !== 0) {
-        filterOnGenre = filterOnLanguage.filter(filteredBook => persianToEnglish(filteredBook.genre) !== uncheckedGenre.getAttribute('id'));
-        displayBooksOnGenre(filterOnGenre);
+    if(filterOnLanguage.length !== 0) {
+        filterOnGenre = filterOnGenre.filter(filteredBook => persianToEnglish(filteredBook.genre) !== uncheckedGenre.getAttribute('id'));
+
+        if(filterOnGenre.length === 0) {
+            generalFilteredBooks = [];
+            filterOnLanguage.forEach(book => generalFilteredBooks.push(book));
+            displayFilteredBooks(generalFilteredBooks)
+        } 
+        else {
+            generalFilteredBooks = generalFilteredBooks.filter(filteredBook => persianToEnglish(filteredBook.genre) !== uncheckedGenre.getAttribute('id'));
+            displayFilteredBooks(generalFilteredBooks);
+        }
+         
     } else {
         filterOnGenre = filterOnGenre.filter(filteredBook => persianToEnglish(filteredBook.genre) !== uncheckedGenre.getAttribute('id'));
-        displayBooksOnGenre(filterOnGenre);
+        generalFilteredBooks = filterOnGenre.filter(filteredBook => persianToEnglish(filteredBook.genre) !== uncheckedGenre.getAttribute('id'));
+        displayFilteredBooks(generalFilteredBooks);
     }
     
 }
+// For genre filters::end
+
+// controlling unchecked inputs for genre and language filters::end
 
 
 
-function displayBooksOnGenre(filterOnGenre) {
-    switch (filterOnGenre.length) {
+
+
+
+
+
+
+// view
+function displayFilteredBooks(filteredBooks) {
+    switch (filteredBooks.length) {
         case 0:
             LIBRARY.innerHTML = '';
             renderCards();
             break;
 
         default:
-            const displayedBooksOnGenre = filterOnGenre.map(book => {
+            const displayedBooksOnGenre = filteredBooks.map(book => {
                 return `   
                 <div class="library__cards">
                     <div class="library__cards--bookmoc">
@@ -189,66 +281,10 @@ function displayBooksOnGenre(filterOnGenre) {
             LIBRARY.innerHTML = '';
             LIBRARY.innerHTML = displayedBooksOnGenre.join('');
             break;
-    }
+        }
 }
-
-
-
-
-function displayBooksOnLanguage(filterOnLanguage) {
-    switch (filterOnLanguage.length) {
-        case 0:
-            switch (filterOnGenre.length === 0) {
-                case true: 
-                     LIBRARY.innerHTML = '';
-                     renderCards();
-                     break;
-
-                case false:
-                    displayBooksOnGenre();
-                    break;     
-            }
-
-
-        default:
-            const displayedBooksOnLanguage = filterOnLanguage.map(book => {
-                return `   
-                <div class="library__cards">
-                    <div class="library__cards--bookmoc">
-
-                           <div class="library__cards--bookmoc--image">
-                           <img class="library__cards--bookmoc--image--frame" src="./assets/images/${book.imgSrc}" alt="">
-                           </div>
-            
-                    </div>
-
-
-                     <div class="library__cards__content">
-                            <h3 class="library__cards__content--title fs-lg-20px mb-2 mt-3">${book.title}</h3>
-                            <h4 class="library__cards__content--author mb-3 fw-normal fs-lg-16px">${book.author}</h4>
-           
-                            <div class="library__cards__content__info mb-2 w-100">
-                                <h5 class="library__cards__content__info--published-date card-info fs-lg-14px fw-light">${book.published_date}</h5>
-                                <h5 class="library__cards__content__info--genre card-info fs-lg-14px fw-light">${book.genre}</h5>
-                                <h5 class="library__cards__content__info--language card-info fs-lg-14px fw-light">${book.language}</h5>
-                            </div>
-           
-                            <div class="library__cards__content__buttons">
-                                <button class="library__cards__content__buttons--add">افزودن به کتابخانه</button>
-                                <div class="library__cards__content__buttons--fav ">
-                                    <i class="fa-regular fa-heart"></i>
-                                </div>
-                            </div>
-                     </div>
-                    </div>
-                </div>`
-            })
-
-            LIBRARY.innerHTML = '';
-            LIBRARY.innerHTML = displayedBooksOnLanguage.join('');
-            break;
-    }
-}
+    
+    
 
 
 
