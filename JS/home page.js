f8628e54527515bebe02c18ab2dc7718a425e0c6
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2023-10-13 13:11:05 +0330

    home page swiper added

--- JS/home page.js
+++ JS/home page.js
@@ -1,8 +1,12 @@
+
+
+
 function sliderMaker() {
     const topSwiperSlide = genres.map(genre => {
-        return `<swiper-slide class="d-flex justify-content-center align-items-center h-90 HOME_CARD_SWIPER" data-genre="${persianToEnglish(genre)}">
-        <div class="cards p-0 box-shadow-none m-3 d-flex align-items-center justify-content-between h-85 w-50 w-max-none">
+        return `<swiper-slide class="d-flex justify-content-center align-items-center h-90 " data-genre="${genre}">
+        <div class="cards p-0 box-shadow-none m-3 d-flex align-items-center justify-content-center h-85 w-50 w-max-none HOME_CARD_SWIPER">
 
+            
             <div class="cards__layout h-100 w-100 d-flex justify-content-center align-items-center position-relative">
 
                 <div class="cards--bookmoc--image cards__layout--image">
@@ -10,39 +14,11 @@ function sliderMaker() {
                 </div>
 
                 <div class="position-absolute text-shadow">
-                    <span class="fs-lg-40px fw-bolder text-white">${genre}<span/>
+                    <span class="fs-lg-40px fw-bolder text-white">${genre}</span>
                 </div>
 
             </div>
-
-            
-
-            <div class="cards__info HOME_CARD_SWIPER_INFO d-none">
-
-                <div class="cards__info--tw d-flex flex-column align-items-center justify-content-between">
-                    <span class="cards__info--tw--title fs-lg-20px fw-bold">ویس و رامین</span>
-                    <span class="cards__info--tw--writer fs-lg-18px fw-semibold">هرچی</span>
-                </div>
-
-
-                <div class="cards__info--lgd d-flex align-items-center justify-content-between">
-
-                    <span class="cards__info--lgd--language fs-lg-16px fw-normal">فارسی</span>
-                    <span class="cards__info--lgd--genre fs-lg-16px fw-normal">ادبیات</span>
-                    <span class="cards__info--lgd--date fs-lg-16px fw-normal">۶۷۰</span>
-                </div>
-
-
-                <div class="cards__content__buttons CARD_BUTTONS_CONTAINER">
-
-                    <button class="cards__content__buttons--add Mybtn ADD_TO_LIBRARY_BTN" data-book-id="">افزودن به کتابخانه</button>
-                    
-                    <i class="fa-regular fa-heart cards__content__buttons--fav MyIcon ADD_TO_FAV_BTN" data-fav-book=""></i>
-                    
-                </div>
-
-            </div>
-
+             
         </div>
     </swiper-slide>
 `
@@ -52,13 +28,76 @@ function sliderMaker() {
 }
 
 
+let parent;
+let initialCardize;
+let cardContentChanged = false;
+let CARD_INTERCHANGABLE_CONTENT;
+
+function resizeCard(event) { 
+    debugger
+    parent = event.target.parentElement;
+    const genre = parent.getAttribute('data-genre'); 
+    event.target.classList.remove('cards')
+    event.target.innerHTML = ''
+    const effectCardsContainer = document.createElement('swiper-container');
+    effectCardsContainer.classList.add("mySwiper");
+    effectCardsContainer.classList.add("h-85");
+    effectCardsContainer.classList.add("w-85");
+    effectCardsContainer.setAttribute("effect", "cards");
+    effectCardsContainer.setAttribute("grab-cursor", "true"); 
+    event.target.appendChild(effectCardsContainer);
+    effectCardsContainer.innerHTML = efectCardsGenerator(genre);
+    
+}
+
+
+function efectCardsGenerator(genre) {
+    const filteredBooks = BOOKS.filter(book => book.genre === genre);
+    const card = filteredBooks.map(book => {
+         return`
+        <swiper-slide>
+            <div class="cards--bookmoc--image cards__layout--image">
+                <img class="cards--bookmoc--image--frame" src="./assets/images/${book.imgSrc}" alt="">
+            </div>
+        </swiper-slide>`
+    })
+
+    return card.join('');
+    
+}
+
+function resizeCard2(event) {
+    debugger
+    const genre = parent.getAttribute('data-genre');
+    event.target.innerHTML = `
+    <div class="cards__layout h-100 w-100 d-flex justify-content-center align-items-center position-relative">
+
+        <div class="cards--bookmoc--image cards__layout--image">
+            <img class="cards--bookmoc--image--frame" src="./assets/images/${persianToEnglish(genre)}.jpg" alt="">
+        </div>
+
+        <div class="position-absolute text-shadow">
+            <span class="fs-lg-40px fw-bolder text-white">${genre}</span>
+        </div>
+
+    </div>`
+   
+    
+    
+}
+
+
+function eventsOnCards(cards) {
+    cards.map(card => card.addEventListener('mouseenter', resizeCard));
+    cards.map(card => card.addEventListener('mouseleave', resizeCard2))
+}
+
 
 function getSlider() {
     const HOME_CARD_SWIPER = Array.from(document.querySelectorAll('.HOME_CARD_SWIPER'));
-    // HOME_CARD_SWIPER.map(card => card.addEventListener('mouseenter', ()=> {
-    //     console.log(event.target.getAttribute('data-genre'))
-    // }))
-    
+    SWIPER_CONTAINER = document.querySelector('.SWIPER_CONTAINER');
+    const CARD_INTERCHANGABLE_CONTENT = document.querySelector('.CARD_INTERCHANGABLE_CONTENT');
+    eventsOnCards(HOME_CARD_SWIPER);
 }
 
 const homeSliderPromise = new Promise((resolve, reject) => {
@@ -71,7 +110,8 @@ const homeSliderPromise = new Promise((resolve, reject) => {
 homeSliderPromise
     .then(sliderMaker)
     .then(getSlider)
-    .then()
+    
+    
 
 
 
